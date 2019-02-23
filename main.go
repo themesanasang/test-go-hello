27a0f1d71ea0675e976f46e0afa91cf06b61f24e
themesanasang <chakrit.cpe@gmail.com>
2019-02-23 17:27:39 +0700

    new  main

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"os"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
+
+func getPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+	   port = "1323"
+	   fmt.Println("No Port In Heroku" + port)
+	}
+	return ":" + port 
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(getPort()))
 }
